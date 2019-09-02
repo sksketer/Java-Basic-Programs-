61a3ee2bbe37510f67076f0e6dd5e463aa42109e
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2019-09-02 07:15:49 -0400

    Add files via upload

---
+++ additionprogram.java
@@ -0,0 +1,13 @@
+public class Sum
+{ 
+    public int sum(int x, int y) 
+    { 
+        return (x + y); 
+    } 
+ 
+    public static void main(String args[]) 
+    { 
+        Sum s = new Sum(); 
+        System.out.println(s.sum(10, 20)); 
+    } 
+} 
